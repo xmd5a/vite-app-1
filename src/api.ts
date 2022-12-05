1880e812e4ee0f24ed3bf693e01ad2a5778addf1
xmd5a <zakaru@gmail.com>
2022-12-05 19:15:37 +0100

    add /api prefix

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ImagePayload } from "./types";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL + "/api";
 
 const getImages = () => {
   return axios.get(`${API_URL}/images`).then((response) => response.data);
